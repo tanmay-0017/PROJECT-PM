4a0802f8ccb2f2d5b6e4c7760cfe4e1e07c20136
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-06-26 17:08:18 +0530

    updated attendant schema

---
+++ Models/Attendant.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+
+const attendantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['available', 'assigned'],
+        default: 'available'
+    }
+},
+{
+    timestamps: true
+});
+
+export const Attendant = mongoose.Schema('Attendant', attendantSchema);
