7d551f9a94e32bd7952fb9e2abbb0a0945832930
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-07-04 00:20:46 +0530

    updated service request

--- Controllers/serviceRequestController.js
+++ Controllers/serviceRequestController.js
@@ -3,53 +3,64 @@ import Customer from '../Models/customer.js';
 import Service from '../Models/Service.js';
 import ServicePerson from '../Models/ServicePerson.js';
 
-
 export const requestService = async (req, res) => {
     const { name, mobileNo, customerId, type, projectName } = req.body;
 
-    const customer = Customer.findOne({"customerId" : customerId});
+    try {
+        const customer = await Customer.findOne({ customerId: customerId });
 
-    if (!customer) {
-        res.status(404).json({ message: 'Customer not found' });
+        if (!customer) {
+            return res.status(404).json({ message: 'Customer not found' });
+        }
+
+        const customerCredentials = await Customer.findOne({
+            customerId: customerId,
+            name: name,
+            mobileNo: mobileNo,
+            projectName: projectName
+        });
+
+        if (!customerCredentials) {
+            return res.status(404).json({ message: 'Incorrect Customer Credentials' });
+        }
+
+        const serviceFound = await Service.findOne({ serviceType: type });
+
+        if (!serviceFound) {
+            return res.status(404).json({ message: 'Service Type Not Found' });
+        }
+
+        const availableServicePerson = await ServicePerson.findOneAndUpdate(
+            { status: 'available', type: type },
+            { status: 'assigned' },
+            { new: true }
+        );
+
+        if (!availableServicePerson) {
+            return res.status(404).json({ message: 'No available service person found' });
+        }
+
+        const serviceRequest = await ServiceRequest.create({
+            name,
+            mobileNo,
+            customerId,
+            type,
+            projectName,
+            servicePerson: availableServicePerson._id,
+            servicePersonName: availableServicePerson.name
+        });
+
+        res.status(200).json({
+            name,
+            mobileNo,
+            customerId,
+            type,
+            projectName,
+            servicePerson: availableServicePerson,
+            servicePersonName: availableServicePerson.name
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
-
-    const customerCredentials = Customer.find({"name": name}, {"mobile": mobileNo}, {"projectName": projectName});
-
-    if (!customerCredentials) {
-        res.status(404).json({ message: 'Incorrect Customer Credentials' });
-    }
-
-    const serviceFound = Service.find({"serviceType": type});
-
-    if (!serviceFound) {
-        res.status(404).json({ message: 'Service Type Not Found' });
-    }
-
-    const availableServicePerson = await ServicePerson.findOneAndUpdate(
-        { status: 'available', type: type },
-        { 
-            status: 'assigned' 
-        },
-        { new: true }
-    );
-
-    ServiceRequest.create({
-        name,
-        mobileNo,
-        customerId,
-        type,
-        projectName,
-        servicePerson: availableServicePerson,
-        servicePersonName : availableServicePerson.name
-    });
-
-    res.status(200).json({
-        name,
-        mobileNo,
-        customerId,
-        type,
-        projectName,
-        servicePerson: availableServicePerson,
-        servicePersonName : availableServicePerson.name
-    })
-}
+};
