910ef496f832880ea3af55fe91fb50838c8e6665
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-07-04 12:54:22 +0530

    updated service controller

--- Controllers/serviceController.js
+++ Controllers/serviceController.js
@@ -13,7 +13,7 @@ export const getServices = async (req, res) => {
 // Get a single service by id
 export const getServiceById = async (req, res) => {
     try {
-        const services = await Service.findOne({ id: req.params.id });
+        const services = await Service.findById(req.params.id);
         if (services) {
             res.status(200).json({ serviceType: services.serviceType});
         } else {
@@ -42,12 +42,11 @@ export const createService = async (req, res) => {
 
 // Update an existing service
 export const updateService = async (req, res) => {
-    const { id } = req.params;
     const { serviceType } = req.body;
 
     try {
-        const updatedService = await Service.findOneAndUpdate(
-            { id },
+        const updatedService = await Service.findByIdAndUpdate(
+            req.params.id,
             { serviceType },
             { new: true }
         );
@@ -62,19 +61,8 @@ export const updateService = async (req, res) => {
     }
 };
 
-// Delete a service
 export const deleteService = async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const deletedService = await Service.findOneAndDelete({ id });
-
-        if (!deletedService) {
-            return res.status(404).json({ message: 'Service not found' });
-        }
-
-        res.status(200).json({ message: 'Service deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    const deletedService = await Service.findByIdAndDelete(req.params.id);
+    if (!deletedService) return res.status(404).json({ message: 'Service not found' });
+    res.status(200).json({ message: 'Service deleted' });
 };
