a47ad8f7c4bd4cdab62d2eb625648801a41d44a7
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-07-12 15:08:04 +0530

    added activity log logic

--- Models/customer.js
+++ Models/customer.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 
+
+const logSchema = new mongoose.Schema({
+    projectName: { type: String, required: true },
+    projectLocation: { type: String, required: true },
+    attendant: { type: mongoose.Schema.Types.ObjectId, ref: 'Attendant', required: true },
+    attendantName: { type: String, required: true },
+    team: { type: String, required: true }
+  }, { timestamps: true}, { _id : false });
+
 const customerSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     mobile: { type: String, required: true },
     projectName: {type: String, required: true},
     projectLocation: {type: String, required: true},
-    customerId: {type: String, required: true, unique: true},
+    customerId: {type: String},
     attendant: { type: mongoose.Schema.Types.ObjectId, ref: 'Attendant' },
     attendantName: { type: String, required: true },
-    team: {type: String, required: true}
+    team: {type: String, required: true},
+    timeDuration: {type: String, default: "00 : 00"},
+    notes: {type: String},
+    log: [logSchema]
 },
 {
     timestamps: true
