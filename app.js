6e09ced2861eb9a0e2ff2d27eba510430146b895
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-07-23 16:19:47 +0530

    updated cloudinary cors policy

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
