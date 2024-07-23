307428c9fa5154a37271e7512b0db592d362465b
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-07-23 16:52:00 +0530

    updated cloudinary url

--- Middlewares/multer.middleware.js
+++ Middlewares/multer.middleware.js
@@ -1,15 +1,19 @@
 import multer from "multer";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        const uploadPath = "./public/temp";
+        // Ensure the directory exists
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+        console.log("File received:", file.originalname);
+        cb(null, file.originalname);
     }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+});
+
+export const upload = multer({ storage });
