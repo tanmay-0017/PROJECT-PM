500bda2f015365446c953d1d21fb27d5d6679e68
“shailesh <shaileshkale87730@gmail.com>
2024-08-08 17:19:29 +0530

    adding sales Models update

--- Models/SalesNote.js
+++ Models/SalesNote.js
@@ -1,29 +1,35 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const salesNoteSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   note: {
     type: String,
-    required: true
+    required: true,
   },
   role: {
     type: String,
     required: true,
-    enum: ['Client', 'Manager', 'Super Admin']
+    enum: ["Client", "Manager", "Super Admin", "Sales Executive"],
+  },
+  project: {
+    type: String,
+  },
+  employeeId: {
+    type: String,
   },
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   time: {
     type: String,
-    default: () => new Date().toLocaleTimeString()
-  }
+    default: () => new Date().toLocaleTimeString(),
+  },
 });
 
-const SalesNote = mongoose.model('SalesNote', salesNoteSchema);
+const SalesNote = mongoose.model("SalesNote", salesNoteSchema);
 
 export default SalesNote;
--- router/customerRoutes.js
+++ router/customerRoutes.js
@@ -1,21 +1,22 @@
-console.log('Customer routes loaded');
+console.log("Customer routes loaded");
 
-import express from 'express';
+import express from "express";
 import {
-    createCustomer,
-    deleteCustomer,
-    getCustomerById,
-    getCustomers,
-    updateCustomer,
-    
-} from '../Controllers/customerController.js';
+  ClientNotes,
+  createCustomer,
+  deleteCustomer,
+  getCustomerById,
+  getCustomers,
+  updateCustomer,
+} from "../Controllers/customerController.js";
 
 const router = express.Router();
 
-router.post('/save', createCustomer);
-router.get('/fetch-all', getCustomers);
-router.get('/fetch/:id', getCustomerById);
-router.put('/update/:id', updateCustomer);
-router.delete('/delete/:id', deleteCustomer);
+router.post("/save", createCustomer);
+router.get("/fetch-all", getCustomers);
+router.get("/fetch/:id", getCustomerById);
+router.put("/update/:id", updateCustomer);
+router.delete("/delete/:id", deleteCustomer);
+router.put("/NoteUpdate/:id", ClientNotes);
 
 export default router;
