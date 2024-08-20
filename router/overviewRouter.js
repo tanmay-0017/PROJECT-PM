7f6718b12511c57585778b5bf438a386dfb55be1
“shailesh <shaileshkale87730@gmail.com>
2024-08-20 13:53:54 +0530

    adding overview logic Top Ex and Bar

--- router/overviewRouter.js
+++ router/overviewRouter.js
@@ -5,6 +5,9 @@ import {
   getTotalMeetings,
   DealsClosed,
   onlineEmployStatus,
+  Top_Executive,
+  Bar,
+  getNotes,
 } from "../Controllers/overviewController.js";
 
 const router = express.Router();
@@ -14,5 +17,8 @@ router.get("/channel-visitors", getNumberOfChannelVisitors);
 router.get("/total-meetings", getTotalMeetings);
 router.get("/total-DealsClosed", DealsClosed);
 router.get("/total-status", onlineEmployStatus);
+router.get("/Top_Executive", Top_Executive);
+router.get("/Bar", Bar);
+router.get("/Note", getNotes);
 
 export default router;
